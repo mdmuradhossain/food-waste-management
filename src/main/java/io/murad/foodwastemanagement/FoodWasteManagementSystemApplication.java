8bb963ecc65905128e66108cf96091067cec8d46
mdmuradhossain <mdmuradhossain33@gmail.com>
2024-04-09 15:29:11 +0600

    food-waste-management repo created

---
+++ src/main/java/io/murad/foodwastemanagement/FoodWasteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package io.murad.foodwastemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWasteManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWasteManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/murad/foodwastemanagement/FoodWasteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.murad.foodwastemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWasteManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
